6cbbeeef19cb44cb3079f3655e48297f05539614
Viktor Zhygalski <vzhyhalski@gmail.com>
2024-07-12 18:48:16 +0200

    init next 2 project

---
+++ frontend/src/app.js
@@ -0,0 +1,24 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/common.css";
+import "../styles/index.css";
+import "../styles/auth.css";
+import "../styles/income.css";
+import "../styles/income_expenses.css";
+
+import "bootstrap/dist/js/bootstrap.min.js"
+
+import {Router} from "./router.js";
+
+class App {
+    constructor() {
+        this.router = new Router();
+        window.addEventListener('DOMContentLoaded', this.handleRouteChanging.bind(this));
+        window.addEventListener('popstate', this.handleRouteChanging.bind(this));
+    }
+
+    handleRouteChanging() {
+        this.router.openRoute();
+    }
+}
+
+(new App());
---
+++ frontend/src/router.js
@@ -0,0 +1,159 @@
+import {Registration} from "./components/registration.js";
+import {Login} from "./components/login.js";
+import {Home} from "./components/home.js";
+import {Auth} from "./services/auth.js";
+import {Sidebar} from "./components/sidebar.js";
+
+
+export class Router {
+    constructor() {
+        this.contentElement = document.getElementById('content');
+        this.titleElement = document.getElementById('title');
+
+
+        this.routes = [
+            {
+                route: '#/',
+                title: 'Авторизация',
+                template: 'templates/login.html',
+                load: () => {
+                    new Login();
+                }
+            },
+            {
+                route: '#/registration',
+                title: 'Регистрация',
+                template: 'templates/registration.html',
+                load: () => {
+                    new Registration();
+                }
+            },
+
+            {
+                route: '#/home',
+                title: 'Главная',
+                template: 'templates/home.html',
+                load: () => {
+                    new Home();
+                }
+            },
+            {
+                route: '#/category',
+                title: 'Категории',
+                template: 'templates/category.html',
+                load: () => {
+                }
+            },
+            {
+                route: '#/open_category',
+                title: 'Создание категории расходов',
+                template: 'templates/open_category.html',
+                load: () => {
+                }
+            },
+            {
+                route: '#/kor_category',
+                title: 'Редактирование дохода/расхода',
+                template: 'templates/kor_category.html',
+                load: () => {
+                }
+            },
+            {
+                route: '#/expenses',
+                title: 'Расходы',
+                template: 'templates/expenses.html',
+                load: () => {
+                }
+            },
+
+            {
+                route: '#/income',
+                title: 'Доходы',
+                template: 'templates/income.html',
+                load: () => {
+                }
+            },
+            {
+                route: '#/income_expenses',
+                title: 'Доходы и расходы',
+                template: 'templates/income_expenses.html',
+                load: () => {
+                }
+            },
+            {
+                route: '#/kor_category',
+                title: 'Редактирование категории доходов',
+                template: 'templates/kor_category.html',
+                load: () => {
+                }
+            },
+            {
+                route: '#/kor_expenses',
+                title: 'Редактирование категории расходов',
+                template: 'templates/kor_expenses.html',
+                load: () => {
+                }
+            },
+            {
+                route: '#/operations',
+                title: 'Создание дохода/расхода',
+                template: 'templates/operations.html',
+                load: () => {
+                }
+            },
+
+        ]
+
+    }
+
+    async openRoute() {
+        const urlRoute = window.location.hash.split('?')[0];
+        if (urlRoute === '#/logout') {
+            await Auth.logout();
+            window.location.href = '#/';
+            return;
+        }
+
+        const newRoute = this.routes.find(item => {
+            return item.route === urlRoute;
+        });
+
+        if (!newRoute) {
+            window.location.href = '#/';
+            return;
+        }
+
+
+        const userInfo = Auth.getUserInfo();
+        const accessToken = localStorage.getItem(Auth.accessTokenKey);
+
+        this.contentElement.innerHTML =
+            await fetch(newRoute.template).then(response => response.text());
+
+        this.titleElement.innerText = newRoute.title;
+
+
+        const sidebar = document.getElementById('sidebar');
+        if (sidebar) {
+            sidebar.innerHTML = await fetch('templates/sidebar.html').then(response => response.text());
+            new Sidebar();
+        }
+
+        const profileFullNameElement = document.getElementById('profile-full-name');
+        if (profileFullNameElement) {
+            profileFullNameElement.innerText = userInfo.name + ' ' + userInfo.lastName;
+
+        }
+
+        if (!accessToken && !userInfo) {
+            if (urlRoute !== '#/' && urlRoute !== '#/registration') {
+                location.href = '#/';
+            }
+        }
+
+
+        newRoute.load();
+    }
+
+
+}
